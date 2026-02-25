3b3c8339acc6dac85add56ff400a4d3d16929518
robinhu <hu28@seas.upenn.edu>
2026-02-25 01:54:36 -0500

    update

--- src/MyNode.java
+++ src/MyNode.java
@@ -8,15 +8,11 @@ public class MyNode<T> {
     public MyNode(T item) {
         this.item = item;
     }
-
     public T getItem() { return item; }
-
     public MyNode<T> getLeft() { return left; }
     public void setLeft(MyNode<T> left) { this.left = left; }
-
     public MyNode<T> getRight() { return right; }
     public void setRight(MyNode<T> right) { this.right = right; }
-
     public MyNode<T> getParent() { return parent; }
     public void setParent(MyNode<T> parent) { this.parent = parent; }
 
